55c97eb75406b71b39c8d390bdb4f680819d8787
kfjbooey <130121253+kfjbooey@users.noreply.github.com>
2025-07-17 03:44:45 -0400

    Delete bios directory

--- bios/userScript.js
+++
@@ -1,9 +0,0 @@
-const overlay = document.getElementById('overlay');
-const music = document.getElementById('bgMusic')
-const video = document.getElementById('bgVideo');
-
-overlay.addEventListener('click', () => {
-    overlay.classList.add('hidden');
-    music.play();
-    video.play();
-});
